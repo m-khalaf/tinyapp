9ffabcdbd933a3b097dd16390a779046f7bbb529
m-khalaf <mkhalaf2211@gmail.com>
2022-08-18 00:53:02 +0000

    Install ejs and set as view engine

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8000
+app.set("view engine", "ejs"); //Set ejs as the view engine
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -19,14 +20,6 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-//  });
- 
-//  app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-//  });
 
 app.listen(PORT, ()=>{
   console.log(`Example app listening on port ${PORT}!`);
