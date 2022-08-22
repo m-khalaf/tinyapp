72614ce5c2acfe8b19ac175741d0ae4c3f9d340a
m-khalaf <mkhalaf2211@gmail.com>
2022-08-22 00:55:57 +0000

    Add route to save user info to cookie

--- express_server.js
+++ express_server.js
@@ -8,6 +8,8 @@ app.set("view engine", "ejs"); //Set ejs as the view engine
 app.use(express.urlencoded({ extended: true })); //translates the buffer sent in the body of post request
 app.use(cookieParser());
 
+const users ={};
+
 function generateRandomString() { //generates random 6 charachter string
   let r = (Math.random() + 1).toString(36).substring(6);
   return r;
@@ -31,17 +33,17 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => { //adding a route for/urls and passing variables to the template
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, user_id: req.cookies["user_id"] };
   res.render("urls_index", templateVars)
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars={username: req.cookies["username"]};
+  const templateVars={user_id: req.cookies["user_id"]};
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => { //adding route handler for urls/:id to capture the shortebed URL as a parameter
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"]};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user_id: req.cookies["user_id"]};
   res.render("urls_show", templateVars)
 });
 
@@ -50,8 +52,8 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL); //redirects the short form to the actual long URL
 });
 
-app.get("/register",(req,res)=>{
-  const templateVars={username: req.cookies["username"]};
+app.get("/register",(req,res)=>{ //route to register new user 
+  const templateVars={user_id: req.cookies["user_id"]};
 res.render("urls_newForm",templateVars)
 });
 
@@ -79,8 +81,22 @@ res.redirect("/urls");
 });
 
 app.post("/logout", (req,res)=>{ //route to logout when user logs out
-res.clearCookie("username");//clears the saved cookie
+res.clearCookie("user_id");//clears the saved cookie
 res.redirect("/urls")
+});
+
+app.post("/register", (req,res)=>{
+const userID = generateRandomString();
+const userEmail = req.body.email;
+const userPassword = req.body.password;
+users[userID]={
+  id: userID,
+  email: userEmail,
+  password: userPassword
+}
+
+res.cookie("user_id",users[userID]);
+res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
