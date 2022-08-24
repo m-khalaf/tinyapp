d13fff21e0ae1794f0fe75c1253d467401553b08
m-khalaf <mkhalaf2211@gmail.com>
2022-08-24 11:39:09 +0000

    Replace all urldatabase objects with new one

--- express_server.js
+++ express_server.js
@@ -23,12 +23,8 @@ function getUserByEmail(usersObject, email) {//returns user's object if it exist
 }
 
 const users = {};//empty object to store users information when they register
-const urlDatabase = {//stores URLs in their short and long forms
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
 
-const urlDatabasee = {
+const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
     userID: "aJ48lW",
@@ -44,7 +40,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabasee);
+  res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
@@ -52,12 +48,12 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => { //adding a route for/urls and passing variables to the template
-  const templateVars = { urls: urlDatabasee, user: users[req.cookies["user_id"]] };
+  const templateVars = { urls: urlDatabase, user: users[req.cookies["user_id"]] };
   res.render("urls_index", templateVars)
 });
 
 app.get("/urls/new", (req, res) => {
-  if (req.cookies["user_id"]===undefined){//redirect user to login page before creating a new link
+  if (req.cookies["user_id"] === undefined) {//redirect user to login page before creating a new link
     res.redirect("/login");
   }
   const templateVars = { user: users[req.cookies["user_id"]] };
@@ -65,20 +61,20 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => { //adding route handler for urls/:id to capture the shortebed URL as a parameter
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.cookies["user_id"]]};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL, user: users[req.cookies["user_id"]] };
   res.render("urls_show", templateVars)
 });
 
 app.get("/u/:id", (req, res) => {
-  if (urlDatabase[req.params.id]===undefined){ //sends an error message if link does not exist
+  if (urlDatabase[req.params.id] === undefined) { //sends an error message if link does not exist
     res.send("Link does not exist, please try again");
   }
-  const longURL = urlDatabase[req.params.id]; //saves the corresponding long URL in a variable
+  const longURL = urlDatabase[req.params.id].longURL; //saves the corresponding long URL in a variable
   res.redirect(longURL); //redirects the short form to the actual long URL
 });
 
 app.get("/register", (req, res) => { //route to register new user 
-  if (req.cookies["user_id"]){//redirects user to main page if they are already logged in
+  if (req.cookies["user_id"]) {//redirects user to main page if they are already logged in
     res.redirect("/urls");
   }
   const templateVars = { user: users[req.cookies["user_id"]] };
@@ -86,7 +82,7 @@ app.get("/register", (req, res) => { //route to register new user
 });
 
 app.get("/login", (req, res) => {
-  if (req.cookies["user_id"]){//redirects user to main page if they are already logged in
+  if (req.cookies["user_id"]) {//redirects user to main page if they are already logged in
     res.redirect("/urls");
   }
   const templateVars = { user: users[req.cookies["user_id"]] };
@@ -94,11 +90,14 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  if (req.cookies["user_id"]===undefined){ //prevents user from adding a new link till they sign in
+  if (req.cookies["user_id"] === undefined) { //prevents user from adding a new link till they sign in
     res.send("you are sneaky...log in first before creating a new link");
   }
   let id = generateRandomString();// generates random cahracters and saves it to the short ID
-  urlDatabase[id] = req.body.longURL;
+  urlDatabase[id] = {
+    longURL: req.body.longURL,
+    userID: req.cookies["user_id"]
+  }
   res.redirect(`/urls/${id}`); //redirect to the urls/:id which initiates a get reuquest that renders the page of the url
 });
 
@@ -109,19 +108,19 @@ app.post("/urls/:id/delete", (req, res) => {// add route to delete urls and redi
 });
 
 app.post("/urls/:id/update", (req, res) => {
-  let idTOUpdate = req.params.id;
-  urlDatabase[idTOUpdate] = req.body.longURL;
+  let idToUpdate = req.params.id;
+  urlDatabase[idToUpdate].longURL = req.body.longURL;
   res.redirect("/urls");
 });
 
 app.post("/login", (req, res) => { //route to loging when user signs in
   if (getUserByEmail(users, req.body.email) === null) {
     res.status(403).send('user does not exist');//returns error if user doesnt exist
-  }else if (getUserByEmail(users, req.body.email).password!==req.body.password){
+  } else if (getUserByEmail(users, req.body.email).password !== req.body.password) {
     res.status(403).send('wrong password');//returns error if password is wrong
-  }else {
-  res.cookie("user_id", getUserByEmail(users, req.body.email).id);//passes user id cookie
-  res.redirect("/urls");
+  } else {
+    res.cookie("user_id", getUserByEmail(users, req.body.email).id);//passes user id cookie
+    res.redirect("/urls");
   }
 });
 
